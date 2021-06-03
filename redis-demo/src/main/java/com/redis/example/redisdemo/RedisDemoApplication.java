beb03d6f8255f1eff218e1602a5f5087ca7a202d
monaadelrabea <mona.adel@afaqy.com>
2021-06-03 11:40:21 +0200

    Spring data (redis/mongo)

--- redis-demo/src/main/java/com/redis/example/redisdemo/RedisDemoApplication.java
+++ redis-demo/src/main/java/com/redis/example/redisdemo/RedisDemoApplication.java
@@ -2,10 +2,8 @@ package com.redis.example.redisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class RedisDemoApplication {
 
     public static void main(String[] args) {
